2ad1cf06b649cfa4b5e2438482d6458c8f576bc0
Simon Takita <smtakita@gmail.com>
2020-09-14 10:04:00 -0700

    Add script to create close up video.

--- gen_map_video_close.py
+++ gen_map_video_close.py
@@ -34,6 +34,11 @@ except ImportError as e:
     sys.exit(1)
 
 
+class GenerateError(Exception):
+    pass
+
+
+
 def load_points_file(filename):
     with open(filename) as fd:
         body = fd.read()
@@ -65,6 +70,7 @@ def generate_map_video(background_image, points_json_file, output_file, fps, tst
     x_portal_offset = int(width / 2)
     y_portal_offset = int(height / 2)
 
+    expected_frame_geometry = (width, height, 3)
 
     points, start_time_string = load_points_file(points_json_file)
 
@@ -108,26 +114,30 @@ def generate_map_video(background_image, points_json_file, output_file, fps, tst
     xlast = xpos
     ylast = ypos
     tlast = tpos
+    tstring = ''
+    points_index = 0
 
-    t0 = t1 = t2 = t3 = t4 = t5 = 0
-    s1 = s2 = s3 = s4 = s5 = 0
+    # t0 = t1 = t2 = t3 = t4 = t5 = 0
+    # s1 = s2 = s3 = s4 = s5 = 0
 
     for frame in range(frame_start, frame_finish):
-        t0 = time.time()
+        # t0 = time.time()
         update_period = 1000
         if frame % update_period == 0:
             frame_total = frame + 1
-            # print('%3.2f %d %d' % (frame / fps, frame, frames))
-            print('%3.2f %d %d  a1 = %f, a2 = %f, a3 = %f, a4 = %f, a5 = %f' % (frame / fps, frame, frames, s1 / frame_total, s2 / frame_total, s3 / frame_total, s4 / frame_total, s5 / frame_total))
+            print('%3.2f %d %d' % (frame / fps, frame, frames))
+            # print('%3.2f %d %d  a1 = %f, a2 = %f, a3 = %f, a4 = %f, a5 = %f' % (frame / fps, frame, frames, s1 / frame_total, s2 / frame_total, s3 / frame_total, s4 / frame_total, s5 / frame_total))
 
         current_time = frame / fps
 
-        t1 = time.time()
+        # t1 = time.time()
 
-        while tpos_adj < current_time and len(points) > 0:
-            point = points.pop(0)
+        while tpos_adj < current_time and points_index < len(points):
+            point = points[points_index]
+            points_index += 1
             xpos = point[0]
             ypos = point[1]
+            tstring = point[2]
             tpos = get_timestamp(point[2]) - start_time
             if tpos == tpos_last:
                 tpos_adj += 1/18
@@ -139,24 +149,34 @@ def generate_map_video(background_image, points_json_file, output_file, fps, tst
             ylast = ypos
             tlast = tpos
 
+        # text = '%d fsec=%3.2f xlast=%d ylast=%d tpos=%d tpos_adj=%3.2f timestamp=%s points_index=%d' % (frame, current_time, xlast, ylast, tpos, tpos_adj, tstring, points_index)
+        # print(text)
+
         # position of frame in complete background image
         frame_pos_x = xlast - x_portal_offset
         frame_pos_y = ylast - y_portal_offset
 
-        t2 = time.time()
+        # t2 = time.time()
         frame = copy.copy(image[frame_pos_y:frame_pos_y+height, frame_pos_x:frame_pos_x+width])
+        if frame.shape != expected_frame_geometry:
+            raise GenerateError('Size does not match portal: %s - expected %s' % (frame.shape, expected_frame_geometry))
+        # print(repr(frame.size))
+        # print(repr(frame.size) + ' ' + repr(frame.shape))
+        # sys.exit(0)
 
-        t3 = time.time()
+        # t3 = time.time()
         cv2.circle(frame, (x_portal_offset, y_portal_offset), 15, color, thickness)
-        t4 = time.time()
-        video.write(frame)
-        t5 = time.time()
 
-        s1 += t1 - t0
-        s2 += t2 - t1
-        s3 += t3 - t2
-        s4 += t4 - t3
-        s5 += t5 - t4
+        # cv2.putText(frame, text, (20,40), cv2.FONT_HERSHEY_SIMPLEX, 0.25, color=(255, 0, 0), thickness=1)
+        # t4 = time.time()
+        video.write(frame)
+        # t5 = time.time()
+
+        # s1 += t1 - t0
+        # s2 += t2 - t1
+        # s3 += t3 - t2
+        # s4 += t4 - t3
+        # s5 += t5 - t4
 
     video.release()
 
