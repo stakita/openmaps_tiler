5c2b99a9e12f3d039b1fe67e961c0579ed6f3248
Simon Takita <smtakita@gmail.com>
2022-06-29 15:17:01 -0700

    centralize points file operations.

---
+++ lib/points_file.py
@@ -0,0 +1,26 @@
+import json
+import dateutil.parser as dup
+
+
+def load(filename):
+    with open(filename) as fd:
+        body = fd.read()
+    gps_data = json.loads(body)
+    return gps_data['gps_points'], gps_data['start_time']
+
+
+def get_start_time(points):
+    time_string = points[0][2]
+    return get_timestamp(time_string)
+
+
+def get_finish_time(points):
+    time_string = points[-1][2]
+    return get_timestamp(time_string)
+
+
+def get_timestamp(time_string):
+    dt = dup.parse(time_string)
+    return dt.timestamp()
+
+
