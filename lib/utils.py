500d734c30b725774d8dc4c9278fc784158c0d66
Simon Takita <smtakita@gmail.com>
2022-06-30 14:53:35 -0700

    Add maximize_zoom method

--- lib/utils.py
+++ lib/utils.py
@@ -1,4 +1,11 @@
 from collections import namedtuple
+import logging
+
+from lib import openstreetmaps as osm
+
+logging.basicConfig(level=logging.INFO,
+                    format='(%(threadName)-10s) %(message)-s')
+log = logging.getLogger(__name__)
 
 # Coordinate = namedtuple("Coordinate", "lon lat")
 # TilePoint = namedtuple("TilePoint", "x y zoom")
@@ -6,8 +13,8 @@ from collections import namedtuple
 CoordinateExtents = namedtuple("CoordinateExtents", "lon_max lon_min lat_max lat_min")
 
 
-def calculate_best_zoom_factor(points_list, pad_percent):
-    # extents = get_track_geo_extents(points_list)
+def calculate_best_zoom_factor(points_list, margin_px, output_x_px, output_y_px, ):
+    extents = get_track_geo_extents(points_list)
     
     pass
 
@@ -32,3 +39,34 @@ def get_track_geo_extents(points_iter):
             lon_max = point['lon']
 
     return CoordinateExtents(lon_max, lon_min, lat_max, lat_min)
+
+
+def maximize_zoom(track_extents, output_x_px, output_y_px, boundary_pixels=20, zoom_max=19):
+    log.info('maximize_zoom - track_extents: %s, output: (%d, %d)' % (repr(track_extents), output_x_px, output_y_px))
+
+    # Bounding corners of the bounding box in coordinate space
+    coord_lo = osm.Coordinate(track_extents.lat_min, track_extents.lon_min)
+    coord_hi = osm.Coordinate(track_extents.lat_max, track_extents.lon_max)
+
+    # Loop until either x or y has exceeded zoom for given output dimensions - take previous zoom
+    for zoom in range(zoom_max + 1):
+        pixel_lo = osm.coordinate_to_pixel_point(coord_lo, zoom)
+        pixel_hi = osm.coordinate_to_pixel_point(coord_hi, zoom)
+
+        x_size_pixel = pixel_hi.x - pixel_lo.x
+        y_size_pixel = pixel_hi.y - pixel_lo.y
+        if (x_size_pixel + (2 * boundary_pixels)) >= output_x_px or (y_size_pixel + (2 * boundary_pixels)) >= output_y_px:
+            break
+
+    zoom_target = zoom - 1 # last zoom value
+    log.info('--> zoom_target: %d' % (zoom_target))
+    pixel_lo = osm.coordinate_to_pixel_point(coord_lo, zoom_target)
+    pixel_hi = osm.coordinate_to_pixel_point(coord_hi, zoom_target)
+    x_size_pixel = pixel_hi.x - pixel_lo.x
+    y_size_pixel = pixel_hi.y - pixel_lo.y
+    log.info('--> pixel_lo: %s' % repr(pixel_lo))
+    log.info('--> pixel_hi: %s' % repr(pixel_hi))
+    log.info('--> x_size_pixel:  %f' % (x_size_pixel))
+    log.info('--> y_size_pixel:  %f' % (y_size_pixel))
+
+    return zoom_target
--- test/test_utils.py
+++ test/test_utils.py
@@ -10,26 +10,42 @@ from lib import utils  # pylint: disable=E0401
 
 def test_get_track_geo_extents():
     in_list = [
-        {
-            'lat': 1.0,
-            'lon': 1.0,
-        },
-        {
-            'lat': 9.0,
-            'lon': 9.0,
-        },
-        {
-            'lat': 5.0,
-            'lon': 5.0,
-        },
-        {
-            'lat': 5.0,
-            'lon': 5.0,
-        },        
+        {'lat': 1.0, 'lon': 1.0 },
+        {'lat': 9.0, 'lon': 1.0 },
+        {'lat': 5.0, 'lon': 5.0 },
+        {'lat': 1.0, 'lon': 9.0 },
     ]
     extents = utils.get_track_geo_extents(iter(in_list))
+    assert extents.lon_min == 1.0
+    assert extents.lon_max == 9.0
     assert extents.lat_min == 1.0
     assert extents.lat_max == 9.0
-    assert extents.lat_min == 1.0
-    assert extents.lat_max == 9.0
+
+
+    in_list = [
+        {'lat': 151.20503342941282, 'lon': -33.870868842232625 },
+        {'lat': 151.211802126524, 'lon': -33.85904467277486 },
+    ]
+
+    extents = utils.get_track_geo_extents(iter(in_list))
+
+    assert extents.lon_min == -33.870868842232625
+    assert extents.lon_max == -33.85904467277486
+    assert extents.lat_min == 151.20503342941282
+    assert extents.lat_max == 151.211802126524
+
+
+def test_maximize_zoom():
+    zoom_expected = 17
+
+    in_list = [
+        {'lat': 151.20503342941282, 'lon': -33.870868842232625 },
+        {'lat': 151.211802126524, 'lon': -33.85904467277486 },
+    ]
+    output_x_px = output_y_px = 1000
+    extents = utils.get_track_geo_extents(iter(in_list))
+
+    zoom = utils.maximize_zoom(extents, output_x_px, output_y_px)
+
+    assert zoom == zoom_expected
     
