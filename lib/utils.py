159d11f35958aca19fcd9666f53fd21a83c3f593
Simon Takita <smtakita@gmail.com>
2022-07-01 02:37:27 -0700

    clean up extent handling logic

--- lib/utils.py
+++ lib/utils.py
@@ -1,22 +1,132 @@
 from collections import namedtuple
 import logging
 
-from lib import openstreetmaps as osm
+from lib import openstreetmaps as osm  # pylint: disable=E0401
 
 logging.basicConfig(level=logging.INFO,
                     format='(%(threadName)-10s) %(message)-s')
 log = logging.getLogger(__name__)
 
-# Coordinate = namedtuple("Coordinate", "lon lat")
-# TilePoint = namedtuple("TilePoint", "x y zoom")
-# PixelPoint = namedtuple("PixelPoint", "x y")
-CoordinateExtents = namedtuple("CoordinateExtents", "lon_max lon_min lat_max lat_min")
 
-
-def calculate_best_zoom_factor(points_list, margin_px, output_x_px, output_y_px, ):
-    extents = get_track_geo_extents(points_list)
-    
+class ConversionException(Exception):
     pass
+
+
+class CoordinateExtents:
+
+    def __init__(self, coord1, coord2):
+        if coord1.lat > coord2.lat:
+            self.lat_hi = coord1.lat
+            self.lat_lo = coord2.lat 
+        else:
+            self.lat_hi = coord2.lat
+            self.lat_lo = coord1.lat
+        if coord1.lon > coord2.lon:
+            self.lon_hi = coord1.lon
+            self.lon_lo = coord2.lon
+        else:
+            self.lon_hi = coord2.lon
+            self.lon_lo = coord1.lon             
+        self._hi = osm.Coordinate(self.lon_hi, self.lat_hi)
+        self._lo = osm.Coordinate(self.lon_lo, self.lat_lo)
+
+
+    def hi(self):
+        return self._hi
+
+
+    def lo(self):
+        return self._lo
+
+
+    def __repr__(self):
+        return '<%s _lo:%s, _hi:%s>' % (self.__class__.__name__, repr(self._lo), repr(self._hi))
+
+
+    def __str__(self):
+        return '%s(_lo:%s, _hi:%s)' % (self.__class__.__name__, repr(self._lo), repr(self._hi))
+
+
+    def to_tile_extents(self, zoom):
+        tile_point1 = osm.coordinate_to_tile_point(self._hi, zoom)
+        tile_point2 = osm.coordinate_to_tile_point(self._lo, zoom)
+        return TileExtents(tile_point1, tile_point2)
+
+
+    def to_pixel_extents(self, zoom):
+        pixel_point1 = osm.coordinate_to_pixel_point(self._hi, zoom)
+        pixel_point2 = osm.coordinate_to_pixel_point(self._lo, zoom)
+        return PixelExtents(pixel_point1, pixel_point2)
+
+
+class TileExtents:
+
+    def __init__(self, tile_point1, tile_point2):
+        if tile_point1.zoom != tile_point2.zoom:
+            raise ConversionException('Mismatch in zoom factors')
+        self.zoom = tile_point1.zoom
+
+        if tile_point1.x > tile_point2.x:
+            self.x_hi = tile_point1.x
+            self.x_lo = tile_point2.x
+        else:
+            self.x_hi = tile_point2.x
+            self.x_lo = tile_point1.x
+
+        if tile_point1.y > tile_point2.y:
+            self.y_hi = tile_point1.y
+            self.y_lo = tile_point2.y
+        else:
+            self.y_hi = tile_point2.y
+            self.y_lo = tile_point1.y
+        self._hi = osm.TilePoint(self.x_hi, self.y_hi, self.zoom)
+        self._lo = osm.TilePoint(self.x_lo, self.y_lo, self.zoom)
+
+
+    def hi(self):
+        return self._hi
+
+
+    def lo(self):
+        return self._lo
+
+
+class PixelExtents:
+
+    def __init__(self, pixel_point1, pixel_point2):
+        if pixel_point1.zoom != pixel_point2.zoom:
+            raise ConversionException('Mismatch in zoom factors')
+        self.zoom = pixel_point1.zoom
+
+        if pixel_point1.x > pixel_point2.x:
+            self.x_hi = pixel_point1.x
+            self.x_lo = pixel_point2.x
+        else:
+            self.x_hi = pixel_point2.x
+            self.x_lo = pixel_point1.x
+
+        if pixel_point1.y > pixel_point2.y:
+            self.y_hi = pixel_point1.y
+            self.y_lo = pixel_point2.y
+        else:
+            self.y_hi = pixel_point2.y
+            self.y_lo = pixel_point1.y
+        self._hi = osm.PixelPoint(self.x_hi, self.y_hi, self.zoom)
+        self._lo = osm.PixelPoint(self.x_lo, self.y_lo, self.zoom)
+
+
+    def hi(self):
+        return self._hi
+
+
+    def lo(self):
+        return self._lo
+
+
+    def to_coordinate_extents(self, zoom):
+        coordinate1 = osm.pixel_point_to_coordinate(self._hi, zoom)
+        coordinate2 = osm.pixel_point_to_coordinate(self._lo, zoom)
+        return CoordinateExtents(coordinate1, coordinate2)
 
 
 def get_track_geo_extents(points_iter):
@@ -38,20 +148,19 @@ def get_track_geo_extents(points_iter):
         if point['lon'] > lon_max:
             lon_max = point['lon']
 
-    return CoordinateExtents(lon_max, lon_min, lat_max, lat_min)
+    coord_lo = osm.Coordinate(lon_min, lat_min)
+    coord_hi = osm.Coordinate(lon_max, lat_max)
+    return CoordinateExtents(coord_lo, coord_hi)
 
 
 def maximize_zoom(track_extents, output_x_px, output_y_px, boundary_pixels=20, zoom_max=19):
-    log.info('maximize_zoom - track_extents: %s, output: (%d, %d)' % (repr(track_extents), output_x_px, output_y_px))
-
-    # Bounding corners of the bounding box in coordinate space
-    coord_lo = osm.Coordinate(track_extents.lat_min, track_extents.lon_min)
-    coord_hi = osm.Coordinate(track_extents.lat_max, track_extents.lon_max)
+    log.debug('maximize_zoom - track_extents: %s, output: (%d, %d)' % (repr(track_extents), output_x_px, output_y_px))
 
     # Loop until either x or y has exceeded zoom for given output dimensions - take previous zoom
     for zoom in range(zoom_max + 1):
-        pixel_lo = osm.coordinate_to_pixel_point(coord_lo, zoom)
-        pixel_hi = osm.coordinate_to_pixel_point(coord_hi, zoom)
+        pixel_extents = track_extents.to_pixel_extents(zoom)
+        pixel_lo = pixel_extents.lo()
+        pixel_hi = pixel_extents.hi()
 
         x_size_pixel = pixel_hi.x - pixel_lo.x
         y_size_pixel = pixel_hi.y - pixel_lo.y
@@ -59,14 +168,25 @@ def maximize_zoom(track_extents, output_x_px, output_y_px, boundary_pixels=20, z
             break
 
     zoom_target = zoom - 1 # last zoom value
-    log.info('--> zoom_target: %d' % (zoom_target))
-    pixel_lo = osm.coordinate_to_pixel_point(coord_lo, zoom_target)
-    pixel_hi = osm.coordinate_to_pixel_point(coord_hi, zoom_target)
+    log.debug('--> zoom_target: %d' % (zoom_target))
+    pixel_extents = track_extents.to_pixel_extents(zoom_target)
+    pixel_lo = pixel_extents.lo()
+    pixel_hi = pixel_extents.hi()
     x_size_pixel = pixel_hi.x - pixel_lo.x
     y_size_pixel = pixel_hi.y - pixel_lo.y
-    log.info('--> pixel_lo: %s' % repr(pixel_lo))
-    log.info('--> pixel_hi: %s' % repr(pixel_hi))
-    log.info('--> x_size_pixel:  %f' % (x_size_pixel))
-    log.info('--> y_size_pixel:  %f' % (y_size_pixel))
+    log.debug('--> pixel_lo: %s' % repr(pixel_lo))
+    log.debug('--> pixel_hi: %s' % repr(pixel_hi))
+    log.debug('--> x_size_pixel:  %f' % (x_size_pixel))
+    log.debug('--> y_size_pixel:  %f' % (y_size_pixel))
 
-    return zoom_target
+    # Calculate bounding box extents
+    pixel_boundary_lo = osm.PixelPoint(pixel_lo.x - boundary_pixels, pixel_lo.y - boundary_pixels, zoom_target)
+    pixel_boundary_hi = osm.PixelPoint(pixel_hi.x + boundary_pixels, pixel_hi.y + boundary_pixels, zoom_target)
+
+    coordinate_boundary_lo = osm.pixel_point_to_coordinate(pixel_boundary_lo)
+    coordinate_boundary_hi = osm.pixel_point_to_coordinate(pixel_boundary_hi)
+
+    boundary_extents = CoordinateExtents(coordinate_boundary_lo, coordinate_boundary_hi)
+
+    return zoom_target, boundary_extents
+
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import math
   
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
@@ -16,36 +17,42 @@ def test_get_track_geo_extents():
         {'lat': 1.0, 'lon': 9.0 },
     ]
     extents = utils.get_track_geo_extents(iter(in_list))
-    assert extents.lon_min == 1.0
-    assert extents.lon_max == 9.0
-    assert extents.lat_min == 1.0
-    assert extents.lat_max == 9.0
+    assert math.isclose(extents.lo().lon, 1.0)
+    assert math.isclose(extents.hi().lon, 9.0)
+    assert math.isclose(extents.lo().lat, 1.0)
+    assert math.isclose(extents.hi().lat, 9.0)
 
 
     in_list = [
-        {'lat': 151.20503342941282, 'lon': -33.870868842232625 },
-        {'lat': 151.211802126524, 'lon': -33.85904467277486 },
+        {'lat': -33.870868842232625, 'lon': 151.20503342941282 },
+        {'lat': -33.85904467277486, 'lon': 151.211802126524 },
     ]
 
     extents = utils.get_track_geo_extents(iter(in_list))
 
-    assert extents.lon_min == -33.870868842232625
-    assert extents.lon_max == -33.85904467277486
-    assert extents.lat_min == 151.20503342941282
-    assert extents.lat_max == 151.211802126524
+    assert math.isclose(extents.lo().lat, -33.870868842232625)
+    assert math.isclose(extents.hi().lat, -33.85904467277486)
+    assert math.isclose(extents.lo().lon, 151.20503342941282)
+    assert math.isclose(extents.hi().lon, 151.211802126524)
 
 
 def test_maximize_zoom():
-    zoom_expected = 17
+    zoom_expected = 16
 
     in_list = [
-        {'lat': 151.20503342941282, 'lon': -33.870868842232625 },
-        {'lat': 151.211802126524, 'lon': -33.85904467277486 },
+        {'lat': -33.870868842232625, 'lon': 151.20503342941282 },
+        {'lat': -33.85904467277486, 'lon': 151.211802126524 },
     ]
     output_x_px = output_y_px = 1000
     extents = utils.get_track_geo_extents(iter(in_list))
 
-    zoom = utils.maximize_zoom(extents, output_x_px, output_y_px)
+    zoom, boundary_extents = utils.maximize_zoom(extents, output_x_px, output_y_px)
 
     assert zoom == zoom_expected
-    
+    assert math.isclose(boundary_extents.lo().lon, 151.20460427597044)
+    assert math.isclose(boundary_extents.lo().lat, -33.87122516577023)
+    assert math.isclose(boundary_extents.hi().lon, 151.21223127996637)
+    assert math.isclose(boundary_extents.hi().lat, -33.85868829839835)
+
+
+# TODO: add tests: extents classes
