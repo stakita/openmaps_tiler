c2b53becbb89eeb4a864b0cafb8118eb87878fcc
Simon Takita <smtakita@gmail.com>
2022-07-01 18:10:33 -0700

    tidy up grid line handling

--- create_overview_video.py
+++ create_overview_video.py
@@ -3,12 +3,13 @@
 create_overview_video.py - Create track overview video from GPX data
 
 Usage:
-  zoom.py <gpx-data> [--output=<filename>] [--tile-cache=<directory>]
+  zoom.py <gpx-data> [--output=<filename>] [--tile-cache=<directory>] [--grid-lines]
 
 Options:
   -h --help                 Show this screen.
   --output=<filename>       Output filename [default: output.avi].
   --tile-cache=<directory>  Tile cache directory [default: tiles].
+  --grid-lines              Add tile lon/lat gridlines to ouptut.
 '''
 # TODO: other options:
 #   pixels_x = output x size in pixels
@@ -81,7 +82,7 @@ def calculate_adjusted_boundary_extents(boundary_extents, zoom_factor, margin_px
     return adjusted_pixel_extents.to_coordinate_extents(zoom_factor), scale_factor
 
 
-def generate_base_background_image(boundary_coord_extents, track_extents, zoom, tile_directory):
+def generate_base_background_image(boundary_coord_extents, track_extents, zoom, tile_directory, draw_grid=False):
     # Download all tiles coverying boundary area
 
     tile_extents = boundary_coord_extents.to_tile_extents(zoom)
@@ -108,98 +109,36 @@ def generate_base_background_image(boundary_coord_extents, track_extents, zoom, 
     log.debug('file_map: ' + repr(file_map))
 
     # Combine into single image
-    for lon_tile in range(tile_ref_lo.x, tile_ref_hi.x + 1):
-        for lat_tile in range(tile_ref_lo.y, tile_ref_hi.y + 1):
-            key = (lon_tile, lat_tile)
-            print(lon_tile, lat_tile)
-            im = Image.open(file_map[key]).convert('RGB')
-
-            if True:
-                # grid lines
-                tile_current = osm.TilePoint(lon_tile, lat_tile, zoom)
-                # tile_offset = osm.TilePoint(lon_tile + 1, lat_tile + 1, zoom)
-
-                geo_current = osm.tile_point_to_coordinate(tile_current)
-                # geo_offset = osm.tile_point_to_coordinate(tile_offset)
-
-                dr = ImageDraw.Draw(im)
-                color = ImageColor.getrgb('brown')
-                dr.line([(0, 0), (0, 255)], fill=color, width=1)
-                dr.line([(0, 0), (255, 0)], fill=color, width=1)
-
-                font = ImageFont.load_default()
-
-                lon_deg_min = geo_current.lon
-                lat_deg_min = geo_current.lat
-                # lon_deg_max = geo_offset.lon
-                # lat_deg_max = geo_offset.lat
-                dr.text([(127, 10)], '%f' % lat_deg_min, font=font, fill=color)
-                dr.text([(10, 127)], '%f' % lon_deg_min, font=font, fill=color)
-
-                color = ImageColor.getrgb('black')
-
-                # if xbtl > lon_tile and xbtl < lon_tile + 1:
-                #     print('xbtl:', xbtl)
-                #     print('lon_tile:', lon_tile)
-                #     x_offset = (xbtl - math.floor(xbtl)) * 256
-                #     dr.line([(x_offset, 0), (x_offset, 255)], fill=color, width=1)
-
-                # if  ybtl > lat_tile and ybtl < lat_tile + 1:
-                #     print('ybtl:', ybtl)
-                #     print('lat_tile:', lat_tile)
-                #     y_offset = (ybtl - math.floor(ybtl)) * 256
-                #     dr.line([(0, y_offset), (255, y_offset)], fill=color, width=1)
-
-                # if xbth > lon_tile and xbth < lon_tile + 1:
-                #     print('xbth:', xbth)
-                #     print('lon_tile:', lon_tile)
-                #     x_offset = (xbth - math.floor(xbth)) * 256
-                #     dr.line([(x_offset, 0), (x_offset, 255)], fill=color, width=1)
-
-                # if ybth > lat_tile and ybth < lat_tile + 1:
-                #     print('ybth:', ybth)
-                #     print('lat_tile:', lat_tile)
-                #     y_offset = (ybth - math.floor(ybth)) * 256
-                #     dr.line([(0, y_offset), (255, y_offset)], fill=color, width=1)
-
-                # color = ImageColor.getrgb('red')
-
-                # if xesl > lon_tile and xesl < lon_tile + 1:
-                #     print('xesl:', xesl)
-                #     print('lon_tile:', lon_tile)
-                #     x_offset = (xesl - math.floor(xesl)) * 256
-                #     dr.line([(x_offset, 0), (x_offset, 255)], fill=color, width=1)
-
-                # if  yesl > lat_tile and yesl < lat_tile + 1:
-                #     print('yesl:', yesl)
-                #     print('lat_tile:', lat_tile)
-                #     y_offset = (yesl - math.floor(yesl)) * 256
-                #     dr.line([(0, y_offset), (255, y_offset)], fill=color, width=1)
-
-                # if xesh > lon_tile and xesh < lon_tile + 1:
-                #     print('xesh:', xesh)
-                #     print('lon_tile:', lon_tile)
-                #     x_offset = (xesh - math.floor(xesh)) * 256
-                #     dr.line([(x_offset, 0), (x_offset, 255)], fill=color, width=1)
-
-                # if yesh > lat_tile and yesh < lat_tile + 1:
-                #     print('yesh:', yesh)
-                #     print('lat_tile:', lat_tile)
-                #     y_offset = (yesh - math.floor(yesh)) * 256
-                #     dr.line([(0, y_offset), (255, y_offset)], fill=color, width=1)
-
-            im.save(file_map[key])
-
     im_full = None
     for lon_tile in range(tile_ref_lo.x, tile_ref_hi.x + 1):
         im_row = None
         for lat_tile in range(tile_ref_lo.y, tile_ref_hi.y + 1):
             key = (lon_tile, lat_tile)
             print(lon_tile, lat_tile)
+
+            im = Image.open(file_map[key]).convert('RGB')
+
+            if draw_grid:
+                # Add lon/lat grid lines to tiles for debugging
+                tile_current = osm.TilePoint(lon_tile, lat_tile, zoom)
+                geo_current = osm.tile_point_to_coordinate(tile_current)
+
+                dr = ImageDraw.Draw(im)
+                color = ImageColor.getrgb('brown')
+                dr.line([(0, 0), (0, 255)], fill=color, width=1)
+                dr.line([(0, 0), (255, 0)], fill=color, width=1)
+
+                lon_deg_min = geo_current.lon
+                lat_deg_min = geo_current.lat
+
+                font = ImageFont.load_default()
+                dr.text([(127, 10)], '%f' % lat_deg_min, font=font, fill=color)
+                dr.text([(10, 127)], '%f' % lon_deg_min, font=font, fill=color)
+
             if im_row is None:
-                im_row = Image.open(file_map[key]).convert('RGB')
+                im_row = im
             else:
-                im_join = Image.open(file_map[key]).convert('RGB')
+                im_join = im
                 im_row = utils.join_images_vertical(im_row, im_join)
 
         if im_full is None:
@@ -207,14 +146,54 @@ def generate_base_background_image(boundary_coord_extents, track_extents, zoom, 
         else:
             im_full = utils.join_images_horizontal(im_full, im_row)
 
-    output_file = 'bozo'
-    im_full.save(output_file + '.raw.png')
+    if draw_grid:
+        # Draw boundary lines
+        dr = ImageDraw.Draw(im_full)
+        im_width, im_height = im_full.size
+        color = ImageColor.getrgb('black')
+
+        x_offset = (tile_lo.x - math.floor(tile_ref_lo.x)) * 256
+        dr.line([(x_offset, 0), (x_offset, im_height)], fill=color, width=1)
+
+        y_offset = (tile_lo.y - math.floor(tile_ref_lo.y)) * 256
+        dr.line([(0, y_offset), (im_width, y_offset)], fill=color, width=1)
+
+        x_offset = (tile_hi.x - math.floor(tile_ref_lo.x)) * 256
+        dr.line([(x_offset, 0), (x_offset, im_height)], fill=color, width=1)
+
+        y_offset = (tile_hi.y - math.floor(tile_ref_lo.y)) * 256
+        dr.line([(0, y_offset), (im_width, y_offset)], fill=color, width=1)
+
+        # Draw track extent lines
+        color = ImageColor.getrgb('red')
+
+        track_tile_extents = track_extents.to_tile_extents(zoom)
+        track_lo = track_tile_extents.lo() 
+        track_hi = track_tile_extents.hi()
+
+        x_offset = (track_lo.x - math.floor(tile_ref_lo.x)) * 256
+        dr.line([(x_offset, 0), (x_offset, im_height)], fill=color, width=1)
+
+        y_offset = (track_lo.y - math.floor(tile_ref_lo.y)) * 256
+        dr.line([(0, y_offset), (im_width, y_offset)], fill=color, width=1)
+
+        x_offset = (track_hi.x - math.floor(tile_ref_lo.x)) * 256
+        dr.line([(x_offset, 0), (x_offset, im_height)], fill=color, width=1)
+
+        y_offset = (track_hi.y - math.floor(tile_ref_lo.y)) * 256
+        dr.line([(0, y_offset), (im_width, y_offset)], fill=color, width=1)
+
+
+    # output_file = 'bozo'
+    # im_full.save(output_file + '.raw.png')
+    return im_full
 
 
 def main(args):
     gpx_filename = args['<gpx-data>']
     output_file = args['--output']
     tile_directory = args['--tile-cache']
+    grid_lines = args['--grid-lines']
 
     margin_pixels = 10
     pixels_x = 1022
@@ -233,11 +212,11 @@ def main(args):
     zoom, boundary_extents = utils.maximize_zoom(track_extents, pixels_x, pixels_y, margin_pixels)
 
     # Calculate expanded aboundary extents
-    adjusted_boundary_extents, scale_factor = calculate_adjusted_boundary_extents(boundary_extents, zoom, margin_pixels, pixels_x, pixels_y)
+    adjusted_boundary_extents, final_scale_factor = calculate_adjusted_boundary_extents(boundary_extents, zoom, margin_pixels, pixels_x, pixels_y)
     print('adjusted boundary extents: %r' % adjusted_boundary_extents)
 
     # Generate base background image
-    generate_base_background_image(adjusted_boundary_extents, track_extents, zoom, tile_directory)
+    generate_base_background_image(adjusted_boundary_extents, track_extents, zoom, tile_directory, grid_lines)
 
     # Generate background image
         # Get track bounding points
