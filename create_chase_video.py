b9522b3b6bd5d36442d7fa9b41c18327804519e5
Simon Takita <smtakita@gmail.com>
2022-07-02 20:49:06 -0700

    get video generation working

--- create_chase_video.py
+++ create_chase_video.py
@@ -7,7 +7,7 @@ Usage:
 
 Options:
   -h --help                 Show this screen.
-  --output=<filename>       Output filename [default: output.avi].
+  --output=<filename>       Output filename [default: output.mp4].
   --tile-cache=<directory>  Tile cache directory [default: tiles].
   --viewport-x=<pixels>     Output video viewport x dimension pixels [default: 1022].
   --viewport-y=<pixels>     Output video viewport x dimension pixels [default: 1022].
@@ -36,8 +36,9 @@ try:
     from PIL import Image
     from PIL import ImageDraw, ImageColor, ImageFont
     from cv2 import cv2
+    import numpy as np
 except ImportError as e:
-    installs = ['docopt', 'Pillow', 'opencv-python']
+    installs = ['docopt', 'Pillow', 'opencv-python', 'numpy']
     sys.stderr.write('Error: %s\nTry:\n    pip install --user %s\n' % (e, ' '.join(installs)))
     sys.exit(1)
 
@@ -58,29 +59,12 @@ def load_gpx_data(gpx_filename):
 def download_tiles(gpx_data, zoom_factor, viewport_offsets, tile_directory):
     file_map = {}
     
-    for coordinate in map(lambda p: osm.Coordinate(p['lon'], p['lat']), gpx_data.all_points()):
+    for coordinate in gpx.gpx_points_to_coordinates(gpx_data.all_points()):
         pixel_point = osm.coordinate_to_pixel_point(coordinate, zoom_factor)
 
-        # Calculate pixel offset bounding points
-        pixel_lo = osm.PixelPoint(
-                        pixel_point.x + viewport_offsets.x_lo,
-                        pixel_point.y + viewport_offsets.y_lo,
-                        zoom_factor
-        )
-        pixel_hi = osm.PixelPoint(
-                        pixel_point.x + viewport_offsets.x_hi,
-                        pixel_point.y + viewport_offsets.y_hi,
-                        zoom_factor
-        )
-        # Convert to tile references
-        tile_lo = osm.tile_reference(osm.pixel_point_to_tile_point(pixel_lo))
-        tile_hi = osm.tile_reference(osm.pixel_point_to_tile_point(pixel_hi))
-
-        # Iterate through all tiles in the bounded region
-        for x in range(tile_lo.x, tile_hi.x + 1):
-            for y in range(tile_lo.y, tile_hi.y + 1):
-                key = (x, y)
-                tile = osm.TilePoint(x, y, zoom_factor)
+        tiles_in_view = get_tiles_in_viewport(pixel_point, viewport_offsets)
+        for tile in tiles_in_view:
+                key = (tile.x, tile.y)
                 output_filename = get_tile_path(tile, tile_directory)
                 file_map[key] = output_filename
                 if not os.path.exists(output_filename):
@@ -88,6 +72,30 @@ def download_tiles(gpx_data, zoom_factor, viewport_offsets, tile_directory):
 
     log.info('file_map: %r' % file_map)
 
+
+def get_tiles_in_viewport(pixel_point, viewport_offsets):
+    tile_set = []
+    # Calculate pixel offset bounding points
+    pixel_lo = osm.PixelPoint(
+                    pixel_point.x + viewport_offsets.x_lo,
+                    pixel_point.y + viewport_offsets.y_lo,
+                    pixel_point.zoom
+    )
+    pixel_hi = osm.PixelPoint(
+                    pixel_point.x + viewport_offsets.x_hi,
+                    pixel_point.y + viewport_offsets.y_hi,
+                    pixel_point.zoom
+    )
+    # Convert to tile references
+    tile_lo = osm.tile_reference(osm.pixel_point_to_tile_point(pixel_lo))
+    tile_hi = osm.tile_reference(osm.pixel_point_to_tile_point(pixel_hi))
+    for x in range(tile_lo.x, tile_hi.x + 1):
+        for y in range(tile_lo.y, tile_hi.y + 1):
+            tile = osm.TilePoint(x, y, pixel_point.zoom)
+            tile_set.append(tile)
+
+    return sorted(tile_set)
+    
 
 def get_tile_path(tile_reference, tile_directory):
     return tile_directory +'/' +'tile_%06d_%06d_%02d.png' % (tile_reference.x, tile_reference.y, tile_reference.zoom)
@@ -140,6 +148,126 @@ def draw_track_points(im_background, image_pixel_coords):
     return im_background
 
 
+def generate_map_video(track_pixel_ts_pairs, output_file, tile_directory, viewport_offsets, pixels_x, pixels_y, zoom, fps=25): #, tstart, tfinish):
+    '''
+    Takes a list of tuples indicating track position and time: (PixelPoint(), timestamp)
+    Renders video frames based on position composing frame based on tiles within the viewport.
+    '''
+    im_canvas = np.zeros((pixels_x, pixels_y, 3), np.uint8)
+
+    x_portal_offset = int(pixels_x / 2)
+    y_portal_offset = int(pixels_y / 2)
+
+    start_time = track_pixel_ts_pairs[0][1]
+    finish_time = track_pixel_ts_pairs[-1][1]
+
+    print(start_time)
+    print(finish_time)
+    total_seconds = finish_time - start_time
+    print(total_seconds)
+
+    frame_start = 0
+    frame_finish = int(total_seconds * fps)
+    frames = int(total_seconds * fps)
+
+    print('frame_start: ', frame_start, frame_start / fps)
+    print('frame_finish:', frame_finish, frame_finish / fps)
+
+    color = (40, 40, 255)
+    thickness = 3
+
+    # fourcc = cv2.VideoWriter_fourcc(*'H264')
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    video = cv2.VideoWriter(output_file, fourcc, float(fps), (pixels_x, pixels_y))
+
+    # xpos = int(round(track_points[0][0], 0))
+    # ypos = int(round(track_points[0][1], 0))
+    pixel_pos = track_pixel_ts_pairs[0][0]
+    # TODO: confirm that track_pixel_ts_pairs[0][1] != start_time under some circumstances
+    tpos = track_pixel_ts_pairs[0][1] - start_time 
+    tpos_last = tpos
+    tpos_adj = tpos
+
+    pixel_pos_last = pixel_pos
+
+    # For each frame in the sequence
+    for frame in range(frame_start, frame_finish):
+        update_period = 1000
+        if frame % update_period == 0:
+            print('%3.2f %d %d' % (frame / fps, frame, frames))
+
+        # Determine the time corresponding to the frame
+        current_time = frame / fps
+
+        # If the head of the track points list is less that current time, pop items off the list
+        while tpos_adj < current_time and len(track_pixel_ts_pairs) > 0:
+            pixel_pos, timestamp = track_pixel_ts_pairs.pop(0)
+
+            # xpos = point[0]
+            # ypos = point[1]
+            # xpos = int(round(point[0], 0))
+            # ypos = int(round(point[1], 0))
+
+            tpos = timestamp - start_time
+            if tpos == tpos_last:
+                # Telemetry is recorded at 18Hz, so if we already have a point at this time, skip ahead
+                tpos_adj += 1 / 18
+            else:
+                tpos_last = tpos
+                tpos_adj = tpos
+
+            # xlast = xpos
+            # ylast = ypos
+
+            pixel_pos_last = pixel_pos
+
+        # viewport_tiles = get_tiles_in_viewport(pixel_pos_last, viewport_offsets)
+        # # load all tiles
+        # for tile in viewport_tiles:
+        #     tile_path = get_tile_path(tile, tile_directory)
+
+        #     image = cv2.imread(tile_path)
+        #     large_img[y_offset:y_end,x_offset:x_end] = small_img
+        #     cv2_imshow(large_img)
+
+        # print('pixel_point_last: ' + repr(pixel_pos_last))
+        # print('viewport_tiles: ' + repr(viewport_tiles))
+        image = build_image(pixel_pos_last, viewport_offsets, pixels_x, pixels_y, tile_directory)
+
+        cv_image = np.array(image) 
+        cv_image = cv_image[:, :, ::-1].copy() # Convert RGB to BGR 
+
+        cv2.circle(cv_image, (x_portal_offset, y_portal_offset), 15, color, thickness)
+        video.write(cv_image)
+
+    video.release()
+
+
+def build_image(pixel_position, viewport_offsets, pixels_x, pixels_y, tile_directory):
+    viewport_tiles = get_tiles_in_viewport(pixel_position, viewport_offsets)
+    # print('viewport_tiles: ' + repr(viewport_tiles))
+    # print('pixel_position: ' + repr(pixel_position))
+    # print('coord: ' + repr(osm.pixel_point_to_coordinate(pixel_position)))
+
+    im_view = Image.new(mode="RGB", size=(pixels_x, pixels_y))
+
+    pixel_position_round = osm.pixel_point_round(pixel_position)
+
+    # load all tiles
+    for tile in viewport_tiles:
+        tile_pixel_ref = osm.tile_point_to_pixel_point(tile)
+        tile_path = get_tile_path(tile, tile_directory)
+        tile_offset_x = - viewport_offsets.x_lo - int(pixel_position.x - tile_pixel_ref.x)
+        tile_offset_y = - viewport_offsets.y_lo - int(pixel_position.y - tile_pixel_ref.y)
+        im_tile = Image.open(tile_path)
+
+        im_view.paste(im_tile, (tile_offset_x, tile_offset_y), mask=None)
+
+    
+    # im_view.show()
+    return im_view
+
+
 def main(args):
     gpx_filename = args['<gpx-data>']
     zoom_factor = int(args['<zoom-factor>'])
@@ -156,10 +284,10 @@ def main(args):
 
     # Setup: Pre calculations
     offsets = ViewportOffsets(
-                -(pixels_x / 2),
-                -(pixels_y / 2),
-                (pixels_x / 2),
-                (pixels_y / 2)
+                int(-(pixels_x / 2)),
+                int(-(pixels_y / 2)),
+                int((pixels_x / 2)),
+                int((pixels_y / 2))
     )
 
     # Setup: Load GPX data
@@ -172,9 +300,12 @@ def main(args):
     annotate_tiles(gpx_data, zoom_factor, tile_directory)
 
     # Compose video
+    track_coordinate_ts_pairs = gpx.gpx_points_to_coordinate_timestamp_tuples(gpx_data.all_points())
+    track_pixel_ts_pairs = list(map(lambda t: (osm.coordinate_to_pixel_point(t[0], zoom_factor), t[1]), track_coordinate_ts_pairs))
+    generate_map_video(track_pixel_ts_pairs, output_temp_file, tile_directory, offsets, pixels_x, pixels_y, zoom_factor)
 
     # Copy over temp file to final filename
-    # shutil.move(output_temp_file, output_file)
+    shutil.move(output_temp_file, output_file)
 
 
 if __name__ == '__main__':
